68ecaaf85f554e5ed2006a1c19546302bddd7c04
Elena Berezina <farfalina1908@gmail.com>
2023-03-10 15:53:23 +0200

    made 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryContent = createGalleryEl(galleryItems);
+
+function createGalleryEl() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }
+
+    ).join("");
+}
+gallery.innerHTML = galleryContent;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    localStorage.setItem('videoplayer-current-time'), 1000;
+});
+
+const onPlay = function(data) {
+    const array = localStorage.getItem('videoplayer-current-time')
+    const currentTime = {
+        duration: 61.857,
+        percent: 0.049,
+        seconds: 3.034,
+    };
+    player.setCurrentTime(currentTime);
+};
+
+player.on('play', onPlay);
 
